f87057123cf3e585d16f745e1c3b642e459eea44
Legionnaire <blake.marter@gmail.com>
2019-07-10 14:30:31 -0700

    Updates

---
+++ precache-manifest.9426c83df25197b2a9d808ff107cdad6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7ffa49786fe89cfa5de37616bf8a12",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "ba5c00d4300afd629f5e",
+    "url": "/clicky-game/static/css/main.d0db4a19.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "ba5c00d4300afd629f5e",
+    "url": "/clicky-game/static/js/main.6cdf6282.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
